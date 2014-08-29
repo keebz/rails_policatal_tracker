e6cb32825e0144f64a9d3a0ab97135838e162c06
keebz <keebz@keebz.com>
2014-08-29 12:11:23 -0700

    add the ability to create a new representative.

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -9,7 +9,15 @@ class RepresentativesController < ApplicationController
 		render('representatives/new.html.erb')
 	end
 
-	# def create
-	# end
-	
+	def create
+		@representative = Representative.new(params[:representative])
+	    if @representative.save
+	      flash[:notice] = "You added a new representative!"
+	      redirect_to('/representatives')
+	    else
+	      flash[:notice] = "Something went wrong. Try again."
+	      render('representatives/new.html.erb')
+	    end
+	end
+
 end
