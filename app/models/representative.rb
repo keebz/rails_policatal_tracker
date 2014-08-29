bcbf1e8757c1f5f97139e4921e40817f9cfe44d4
keebz <keebz@keebz.com>
2014-08-29 15:20:29 -0700

    create and pass a test for the representative to belong to a state.

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,4 +1,5 @@
 class Representative <ActiveRecord::Base
 	validates :name, :presence => true
 	validates :party, :presence => true
+	belongs_to :state
 end
--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Representative do
 	it { should validate_presence_of :name}
 	it { should validate_presence_of :party}
+	it { should belong_to :state}
 end
