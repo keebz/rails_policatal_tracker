f92d90b5eed4e8bdb39ac14562f9954fda8a5b62
keebz <keebz@keebz.com>
2014-08-29 14:20:45 -0700

    complete CRUD functions for representatives. Create routes for states.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
   match('/representatives/:id', {:via => [:patch, :put], :to => 'representatives#update'})
   match('representatives/:id/edit', {:via => :get, :to => 'representatives#edit'})
   match('/representatives/:id', {:via => :delete, :to => 'representatives#destroy'})
+
+  match('states', {:via => :get, :to => 'states#index'})
+  match('states', {:via => :post, :to => 'states#create'})
+  match('states/new', {:via => :get, :to => 'states#new'})
+  match('states/:id', {:via => :get, :to => 'states#show'})
+  match('/states/:id', {:via => [:patch, :put], :to => 'states#update'})
+  match('states/:id/edit', {:via => :get, :to => 'states#edit'})
+  match('/states/:id', {:via => :delete, :to => 'states#destroy'})
 end
